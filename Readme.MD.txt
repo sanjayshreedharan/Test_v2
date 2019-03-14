aa49ab0d70778c373aa5f5ba3579b216ee45abb5
Sanjay Shreedharan <shridharsanjay1994@gmail.com>
2019-03-13 20:23:01 -0700

    v3 Commit

--- Readme.MD.txt
+++ Readme.MD.txt
@@ -1,2 +1,2 @@
 This is a test!
-Update v1
+Update v3
--- helloworld.m
+++ helloworld.m
@@ -1,2 +1,14 @@
 disp ('Hello World v2!')
+
+fid = fopen(fullfile('C:\Users\shrid\Desktop\Test_v2', 'log.txt'), 'a');
+
+if fid == -1
+ 
+	error('Cannot open log file.');
+
+end
+fprintf(fid, '%s: %s\n', datestr(now, 0), "Test Worked");
+
+fclose(fid);
+
 exit (0);
